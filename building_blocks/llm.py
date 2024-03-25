c327bce4d0bba3132d560707b27670988c65ae79
gaborm <126419721+GaboRM9@users.noreply.github.com>
2024-03-25 10:11:14 -0600

    changes

--- building_blocks/llm.py
+++ building_blocks/llm.py
@@ -6,21 +6,21 @@ import os
 load_dotenv()
 
 def batch():
-    chat = ChatGroq(temperature=0, model_name="gemma-7b-It", groq_api_key=os.getenv("GROQ_API_KEY"))
+    chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768", groq_api_key=os.getenv("GROQ_API_KEY"))
 
-    system = "Your are a helpful assistant"
+    system = "Eres un asistente amable que llama a prospectos"
     human = "{text}"
     prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
 
     chain = prompt | chat
-    print(chain.invoke({"text": "Explain the importance of low latency LLMs."}))
+    print(chain.invoke({"text": "Agenda una sesión demo para el prospecto."}))
 
 # Streaming
 def streaming():
     chat = ChatGroq(temperature=0, model_name="llama2-70b-4096",groq_api_key=os.getenv("GROQ_API_KEY"))
-    prompt = ChatPromptTemplate.from_messages([("human", "Write a very long poem about {topic}")])
+    prompt = ChatPromptTemplate.from_messages([("human", "Eres un agente de ventas sobre {topic}, tu respuesta debe ser de hasta 15 caracteres")])
     chain = prompt | chat
-    for chunk in chain.stream({"topic": "The Moon"}):
+    for chunk in chain.stream({"topic": "QuestionPro, software de encuestas"}):
         print(chunk.content, end="", flush=True)
 
 # batch()
